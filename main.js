21fe417be0fa28134ce37ce77c6342fdd490b831
Wyatt <wyatthunterallan@gmail.com>
2019-02-21 12:01:06 -0700

    Added lots of JS arrays for star wars

---
+++ main.js
@@ -0,0 +1,9 @@
+var means declaring something to anything I want
+let means 'is another way of declaring a variable'
+a single = is an assignment variable. "Is assigned the value of" Say this in your Head.
+ex. thisVariable = 'kristin'
+const ANSWER = 42
+primitive means a datatype that is buuilt into the langauge and cannot be changed "immutable"
+use the === over ==
+let minArray = []
+
