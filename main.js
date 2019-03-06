915fe6b11f8cd0a30ec3e929de668ad225271d0f
Wyatt <wyatthunterallan@gmail.com>
2019-03-05 22:35:54 -0700

    HTML h1 is now being pushed by main.js and added many different examples to main.js.

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-import { films } from './films.js'
 
-console.log(films[0].title)
-console.log(films[0].opening_crawl)
 
+// function wyatt(a,b) {
+//     return a+b;
+// }
+// alert( wyatt(455,572) )
+// document.getElementById("JSTEST").innerHTML = wyatt(19,45);
+
+// function age(name, age) {
+//     return name + " " + age;
+// }
+// const any = age("Wyatt", 24)
+// alert(any)
+
+// var name = prompt("What is your name?"); var lastName = prompt("What is your last name?"); alert(name + " " + lastName);
+
+
+var age = prompt("How old are you?"); {
+if (age < 16) { 
+    alert("you can't drive, stay off the road loser!"); 
+} else {
+    alert("You can drive, Hooray!! But unfortunately, you're getting older.")
+}
+}
+
+var h1 = "Welcome, my name is Wyatt";
+document.getElementById("JSTEST").innerHTML = h1;
+
+var myList = ["oranges", "apples", "bananas"]
