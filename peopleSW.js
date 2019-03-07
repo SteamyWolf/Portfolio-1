c9c852d92ca74ae255b7f512c3048328eabec682
Wyatt <wyatthunterallan@gmail.com>
2019-03-07 12:29:43 -0700

    Class day peopleSW.js update

--- peopleSW.js
+++ peopleSW.js
@@ -1,3 +1,44 @@
-import { people } from './data/people.js' 
+import { people } from '../data/people.js' 
+import { planets } from '..data/planets.js'
 
-console.log(people)
+const getLastNumber = (url) => {
+    let end = url.lastIndexOf('/')
+    let start = end -2
+    if(url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)    
+}
+
+const allHomeWorlds = people.map(person.map => {
+    let founderWorld = planets.find(planet => {
+        return planet.url === person.homeworld
+    })
+    let imgURL = getLastNumber(person.url)
+    getLastNumber(person.url)
+    return {
+        name: person.name,
+        home: founderWorld.name,
+        eye_color: person.eye_color,
+        imagePath: 
+    }
+})
+
+console.log(allHomeWorlds)
+
+const mainContainer = document.createElement('div')
+mainContainer.classname = 'container'
+
+allHomewWorlds.forEach((person) => 
+    let personElement = document.createElement('div')
+    let planetElement = document.createElement('p')
+    let imgElement = document.createElement('img')
+
+    personElement.className = 'box'
+    personElement.textContent = person.name
+    planetElement.textContent = person.home
+
+    personElement appendChild(planetElement)
+    personElement.appendChild(imageElement)
+    main
+})
