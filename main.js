e711f4e2060e40a24df2dbcb78271e61cdb19617
Wyatt <wyatthunterallan@gmail.com>
2019-03-13 22:47:38 -0600

    Started the Star Wars opening theme onto home page. All in main.js

--- main.js
+++ main.js
@@ -23,10 +23,31 @@ if (age < 16) {
 }
 }
 
-var h1 = "Welcome, my name is Wyatt";
-document.getElementById("JSTEST").innerHTML = h1;
+// var h1 = "Welcome, my name is Wyatt";
+// document.getElementById("JSTEST").innerHTML = h1;
 
-var myList = ["oranges", "apples", "bananas"]
-myList.forEach(function(x) {
-console.log(x)
-});
+// var myList = ["oranges", "apples", "bananas"]
+// myList.forEach(function(x) {
+// console.log(x)
+// });
+
+const numStars = 100
+
+for (let i = 0; i < numStars; i++) {
+    let star = document.createElement("div");
+    star.classname = "star";
+    var xy = getRandomPosition();
+    star.style.top = xy[0] + 'px';
+    star.style.left = xy[1] + 'px';
+    document.body.append(star);
+}
+
+
+function getRandomPosition() {
+    var y = window.innerWidth;
+    var x = window.innerHeight;
+    var randomX = Math.floor(Math.random()*x);
+    var randomY = Math.floor(Math.random()*y);
+    return [randomX, randomY];
+}
+
