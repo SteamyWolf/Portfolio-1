fc129af8d42455108a9e75a3c5f4bef16e13d47a
Wyatt <wyatthunterallan@gmail.com>
2019-03-26 13:09:47 -0600

    Full pokemon Update from class Tuesday 03/26

--- data/pokeCards.js
+++ data/pokeCards.js
@@ -10,7 +10,7 @@ const mainContainer = document.querySelector('.container')
       let title = document.createElement('h2')
       let image = document.createElement('img')
 
-      let upperName = pokeData.name.charAt(0).toUpperCase()
+      let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
       title.textContent = upperName
       image.src = pokeData.sprites.front_shiny
       card.appendChild(title)
