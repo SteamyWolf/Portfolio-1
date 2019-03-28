b64acd9adc0e6c746afc55057a816dc9590fe0aa
Wyatt <wyatthunterallan@gmail.com>
2019-03-28 12:42:21 -0600

    Added pokemon cards. They are working but the pictures are not showing up. Probably a link problem

--- data/pokeCards.js
+++ data/pokeCards.js
@@ -1,31 +1,68 @@
 import { pokemon } from '../data/pokemon.js'
 
-console.log(pokemon)
+class Pokemon {
+  constructor(id) {
+    this.id = id
+  }
+}
 
 const mainContainer = document.querySelector('.container')
 
   function createPokeCard(pokeData) {
       let card = document.createElement('div')
       card.className = 'box'
-      let title = document.createElement('h2')
+      let figure = document.createElement('figure')
+      let caption = document.createElement('figcatpion')
       let image = document.createElement('img')
 
-      let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
-      title.textContent = upperName
+      // let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
+      caption.textContent = pokeData.name
+      if(pokeData.id !== 0) {
+        image.src = `../images/${pokeData.imageID}${pokeData.name}.png`
+      } else {
+        image.src = `../images/pokeball.png`
+      }
       image.src = pokeData.sprites.front_shiny
-      card.appendChild(title)
-      card.appendChild(image)
-    mainContainer.appendChild(card)
+      figure.appendChild(caption)
+      card.appendChild(figure)
+      mainContainer.appendChild(card)
   }
 
-  pokemon.forEach((singleMon) => {
-    fetch(singleMon.url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(myJson) {
-      createPokeCard(myJson);
-    });
-  
-  })
+  // pokemon.forEach((singleMon) => {
+  //   fetch(singleMon.url)
+  //   .then(function(response) {
+  //     return response.json();
+  //   })
+  //   .then(function(myJson) {
+  //     console.log(myJson)
+  //     // createPokeCard(myJson);
+  //   });
+  // })
 
+  function fetchSinglePokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(retreivedPokemon) {
+      console.log(retreivedPokemon.id.length)
+      if(retreivedPokemon.id < 10) {
+        retreivedPokemon.imageID = "00" + retreivedPokemon.id
+      }
+      if(retreivedPokemon.id > 9 && retreivedPokemon.id < 100) {
+        retreivedPokemon.imageID = "0" + retreivedPokemon.id
+      }
+      if(retreivedPokemon.id > 99) {
+        retreivedPokemon.imageID = retreivedPokemon.id
+      }
+      retreivedPokemon.name = retreivedPokemon.name.charAt(0).toUpperCase() + retreivedPokemon.name.slice(1)
+      createPokeCard(retreivedPokemon)
+    })
+  }
+
+const newPokemonButton = document.querySelector('button')
+
+newPokemonButton.addEventListener('click', function(){
+  let pokemonID = prompt('Enter an ID of an existing Pokemon')
+  fetchSinglePokemon(pokemonID)
+})
